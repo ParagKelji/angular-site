c3c2b4a060ca9119376f6eb7df8097de808b8c1b
Parag R Kelji <kelji.parag@gmail.com>
2024-08-26 12:26:34 -0700

    Added a post login GET request and a display screen
    Added a post login GET request and a display screen

--- src/app/auth-content/auth-content.component.ts
+++ src/app/auth-content/auth-content.component.ts
@@ -10,14 +10,53 @@ import { NgFor } from '@angular/common';
   styleUrl: './auth-content.component.css'
 })
 export class AuthContentComponent {
-  data: string[] = [];
+  data: any[] = [];
 
   constructor(private axiosService: AxiosService) {}
 
   ngOnInit(): void {
-    this.axiosService.request("GET","/messages",[])
-    .then(
-      (response) => this.data = response.data
+    console.log("Token value: " + this.axiosService.getAuthToken());
+    this.callFavoriteSports();
+    //this.callMessages();
+  }
+
+  callMessages(): void {
+    /*
+    this.axiosService.axiosGet(
+      "/messages/",
+      null
     );
+    */
+    this.axiosService.request(
+      "GET",
+        "/messages/",
+        {}
+      ).then(
+        (response) => {
+          this.data = response.data
+          console.log(this.data);
+        }
+      ).catch(
+        (reason) => {
+          console.log("Failed due to: " + reason);
+          this.data[0] = reason;
+    });
+  }
+  
+  callFavoriteSports(): void {
+    this.axiosService.request(
+      "GET",
+        "/favoritesports?token=" + this.axiosService.getAuthToken(),
+        {}
+      ).then(
+        (response) => {
+          this.data = response.data
+          console.log(this.data);
+        }
+      ).catch(
+        (reason) => {
+          console.log("Failed due to: " + reason);
+          this.data[0] = reason;
+    });
   }
 }
--- src/app/axios.service.ts
+++ src/app/axios.service.ts
@@ -7,8 +7,9 @@ import axios from 'axios';
 export class AxiosService {
 
   constructor() {
-    axios.defaults.baseURL = "http://localhost:8080"
+    axios.defaults.baseURL = "http://localhost:8090"
     axios.defaults.headers.post["Content-Type"] = "application/json"
+    axios.defaults.headers.get["Content-Type"] = "application/json"
   }
 
   getAuthToken(): string | null {
@@ -27,14 +28,35 @@ export class AxiosService {
     let headers = {};
 
     if (this.getAuthToken() !== null ) {
-      headers = {"Authorization": "Bearer " + this.getAuthToken()};
+      (headers as any)["Authorization"] = "Bearer " + this.getAuthToken(),
+      (headers as any)["Access-Control-Request-Method"] = method
     }
     
+    
+    console.log("data: " + JSON.stringify(data));
+    console.log("window.location.search: " + window.location.search);
     return axios({
       method: method,
       url: url,
       data: data,
       headers: headers
     })
+  }
+
+  axiosGet(url: string, data: any) {
+    if (  data!== null ) {
+      axios.get(url, {
+        params: {
+          data
+        }
+      })
+      .then( (response) => console.log(response))
+      .catch( (error) => console.log(error))
+    } else {
+      axios.get(url)
+      .then( (response) => console.log(response))
+      .catch( (error) => console.log(error))
+    }
+    
   }
 }
